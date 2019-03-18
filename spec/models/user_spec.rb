d2c8b90e1a06b05615338f1c5fdb4ddc9a6bab8e
Mariah Schneeberger <mariah.acacia@gmail.com>
2019-03-18 13:51:15 -0700

    initial commit, gets everything ready to start TDD, starting with User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe User do
+
+  it 'is valid with with valid attributes (email, first name, last name, password, password confirmation)' 
+
+  it 'is not valid with invalid email'
+  it 'is not valid without an email'
+  it 'is not valid with invalid first name'
+  it 'is not valid without a first name'
+  it 'is not valid with invalid last name'
+  it 'is not valid without a last name'
+  it 'is not valid with invalid password'
+  it 'is not valid without a password'
+  it 'is not valid with invalid password confirmation'
+  it 'is not valid without a password confirmation'
+
+
+
+
+
+end
